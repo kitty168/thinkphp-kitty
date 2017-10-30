a7e160d3f2a59fe898ff7f921ee613d44c3e4bb7
kitty.cheng <450038893@qq.com>
2017-10-30 20:07:17 +0800

    整合百度webuploader-1

---
+++ Application/Common/Controller/BaseController.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ * Author: Kitty.Cheng
+ * Date: 2017/10/30
+ * Time: 11:30
+ * Link: www.gyang.net
+ * Email: kitty.cheng@gyang.net
+ */
+
+namespace Common\Controller;
+use Think\Controller;
+
+class BaseController extends Controller
+{
+
+}
